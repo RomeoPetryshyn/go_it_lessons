3d902fc9e0ccc0dc1404823d4138b8f680b209c6
rpetryshyn <rpetryshyn@amplify.com>
2023-02-13 15:43:06 +0200

    [RP] Lesson 4

--- L4/index.js
+++ L4/index.js
@@ -1,5 +1,13 @@
 console.log('Executing index.js script...');
-
+// Q1.5
+// function doStuff() {}; // function declaration
+// const doStuff = function() {} // function expression
+// // Example 2
+// doStuff(); 
+// function doStuff() {}; // помилки не буде
+// // Example 1
+// doStuff(); 
+// const doStuff = function () {}; // помилка буде
 //
 //
 //
@@ -10,23 +18,48 @@ console.log('Executing index.js script...');
 // `24.7` або `24,7`, тобто як роздільник дробової частини
 // може бути кома.
 // Індекс маси тіла необхідно округлити до однієї цифри після коми;
+// function calcBMI(weight, height) {
+//     const heightCorrectStringNumber = height.replace(',', '.');
+//     const heightNum = Number(heightCorrectStringNumber);
+//     const squareHeightNum = Math.pow(heightNum, 2);
+//     const weightCorrectStringNumber = weight.replace(',', '.');
+//     const weightNumber = Number(weightCorrectStringNumber);
+//     const calculatedResult = weightNumber/squareHeightNum
+//     return calculatedResult.toFixed(1);
+// }
 // const bmi = calcBMI('88,3', '1.75');
 // console.log(bmi); // 28.8
 //
 //
 //
 // 2. Будемо писати функцію `min(a,b)`, яка повертає найменше з чисел `a` та `b`.
+// const findMin = function (value1, value2) {
+//     if (value1 < value2) {
+//         return value1;
+//     }
+//     if (value2 < value1) {
+//         return value2;
+//     }
+//     return value1;
+// };
 // console.log(findMin(2, 5)); // 2
 // console.log(findMin(3, -1)); // -1
 // console.log(findMin(1, 1)); // 1
+
 //
 //
 //
 // 3. Будемо писати функцію `getRectArea(dimensions)` для обчислення площі прямокутника
 // зі сторонами, значення яких будуть передані до параметра `dimensions` у вигляді
 // рядка. Значення гарантовано розділені пробілом.
-// function getRectArea(dimensions) {}
+// function getRectArea(dimensions) {
+//     const arrayOfValue = dimensions.split(' ');
+//     const firstElement = Number(arrayOfValue[0]);
+//     const secondElement = Number(arrayOfValue[1]);
+//     return firstElement * secondElement;
+// }
 // console.log(getRectArea('8 11'));
+// console.log('test1-test2-test3-test4'.split('-'));
 //
 //
 //
@@ -36,9 +69,16 @@ console.log('Executing index.js script...');
 // починатися з `1`.
 // Наприклад для першого елемента масиву `['Mango', 'Poly', 'Ajax']` з індексом `0`
 // буде виведено `1 - Mango`, а для індексу 2 виведе `3 - Ajax`.
-// function logItems(items) {}
 // logItems(['Mango', 'Poly', 'Ajax', 'Watermelon', 'Apple']);
 // logItems(['John', 'Jack', 'Jeremy', 'Jake', 'Jackson']);
+
+// function logItems(items) {
+//     for (let i = 0; i < items.length; i++) {
+//         const index = i + 1;
+//         const element = items[i];
+//         console.log(`${index}-${element}`);
+//     }
+// }
 //
 //
 //
@@ -47,16 +87,31 @@ console.log('Executing index.js script...');
 // рядки імен та телефонних номерів, розділені комами. Порядковий номер імен та
 // телефонів у рядках вказують на відповідність. Кількість імен та телефонів
 // гарантовано однакова.
-// function printContactsInfo(names, phones) {}
-// printContactsInfo(
-//   'Jacob,William,Solomon,Artemis',
-//   '89001234567,89001112233,890055566377,890055566300',
-// );
+// const printContactsInfo = function(names, phones) {
+//     const namesArray = names.split(',');
+//     const phonesArray = phones.split(',');
+//     let i = 0;
+//     while (i < phonesArray.length) {
+//         const name = namesArray[i];
+//         const phoneNumber = phonesArray[i];
+//         console.log(`Name: ${name}. Phone Number: ${phoneNumber}`);
+//         i+=1;
+//     }
+// }
+// printContactsInfo('Jacob,William,Solomon,Artemis','89001234567,89001112233,890055566377,890055566300');
 //
 //
 //
 // 6. Будемо писати функцію `findLargestNumber(numbers)`яка шукає найбільше число в масиві.
-// function findLargestNumber(numbers) {}
+// function findLargestNumber(numbers) {
+//     let maxNumber = numbers[0];
+//     for (const number of numbers) {
+//         if (number > maxNumber) {
+//             maxNumber = number;
+//         }
+//     }
+//     return maxNumber;
+// }
 // console.log(findLargestNumber([2, 17, 94, 1, 23, 37])); // 94
 // console.log(findLargestNumber([49, 4, 7, 83, 12])); // 83
 //
@@ -65,15 +120,23 @@ console.log('Executing index.js script...');
 // 7. Будемо писати функцію `calcAverage()` яка приймає довільну кількість аргументів
 // і повертає їхнє середнє значення. Усі аргументи будуть лише числами.
 // function calAverage() {
-//     console.log(arguments);
-//     console.log(arguments.length);
-//     let sum = 0;
-//     const argumentsSize = arguments.length;
-//     for (let index = 0; index < argumentsSize; index++) {
-//         const item = arguments[index];
-//         sum += item;
-//     }
-//     return sum / argumentsSize;
+//     // let sum = 0;
+//     // console.log(args);
+//     // for (let index = 0; index < args; index++) {
+//     //     const item = arguments[index];
+//     //     sum += item;
+//     // }
+//     // return sum / args;
+//     // let sum = 0;
+//     // let i = 0;
+//     // const argumentsSize = arguments.length;
+//     // const argumentsArray = [];
+//     // do {
+//     //     const item = arguments[i];
+//     //     argumentsArray.push(item);
+//     //     i+=1;
+//     // } while (i < argumentsSize);
+//     // console.log(Array.from(arguments));
 // }
 // console.log(calAverage(1, 2, 3, 4)); // 2.5
 // console.log(calAverage(14, 8, 2)); // 8
@@ -90,7 +153,13 @@ console.log('Executing index.js script...');
 // const doubleDigitHours = String(hours).padStart(2, 0);
 // const doubleDigitMinutes = String(minutes).padStart(2, 0);
 // console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
-// function formatTime(minutes) {}
+// function formatTime(totalMinutes) {
+//     const hours = Math.floor(totalMinutes / 60);
+//     const minutes = totalMinutes % 60;
+//     const doubleDigitHours = String(hours).padStart(2, 0);
+//     const doubleDigitMinutes = String(minutes).padStart(2, 0);
+//     return `${doubleDigitHours}:${doubleDigitMinutes}`;
+// }
 // console.log(formatTime(70)); // "01:10"
 // console.log(formatTime(450)); // "07:30"
 // console.log(formatTime(1441)); // "24:01"
@@ -101,6 +170,10 @@ console.log('Executing index.js script...');
 // - `addCourse(name)` - додає курс до кінця колекції
 // - `removeCourse(name)` - видаляє курс із колекції
 // - `updateCourse(oldName, newName)` - змінює ім'я на нове
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// *важливо:| shift(видаляє перший елемент з масиву)     |     pop(видаляє останній елемент з масиву)   |
+//          | unshift(додає елемент на початок масиву)   |     push (додає елемент в кінець масиву),    |
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL'];
 // addCourse('Express');
 // console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
@@ -110,6 +183,66 @@ console.log('Executing index.js script...');
 // removeCourse('Vue'); // 'Курс із таким ім'ям не знайдено'
 // updateCourse('Express', 'NestJS');
 // console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
+// function addCourse(course) {
+//     if (!courses.includes(course)) {
+//         courses.push(course);
+//     } else {
+//         console.log('Ви маєте курс');
+//     }
+// }
+// function removeCourse(course) {
+//     let hasDeletedItem = false;
+//     for (let i = 0; i < courses.length; i++) {
+//         const existingCourse = courses[i];
+//         if (existingCourse === course) {
+//             delete courses[i];
+//             hasDeletedItem = true;
+//         }
+//     }
+//     if (!hasDeletedItem) {
+//         console.log("Курс із таким ім'ям не знайдено");
+//     }
+// }
+// function updateCourse(updateFrom, updateTo) {
+//     const updateFromIndex = courses.indexOf(updateFrom);
+//     courses[updateFromIndex] = updateTo;
+// }
 //
 //
 //
+
+
+
+// Q1.5
+// function calcSumm(a, b) {
+//     if (b === 1) {
+//         return a;
+//     } else {
+//         return a * calcSumm(a, b - 1);
+//     }
+// };
+
+// console.log(calcSumm(2, 3)); // 8
+// step 1: result: 2 * calcSumm(2, 3 - 1); множимо 2 на результат calcSumm(2, 3 - 1)
+// step 2(calcSumm(2, 3 - 1)): 2 * calcSumm(2-1); множимо 2 на результат calcSumm(2-1)
+// step 3(calcSumm(2-1)): результатом є число 2.
+// результат кроку 3 є число 2
+// результат кроку 2 є число 2 помножити на результат кроку 3 (число 2)
+// результат кроку 1 є число 2 помножити на результат кроку 2 (число 2 * 2 * 2)
+
+// 2 * 2 * 2 = 8;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

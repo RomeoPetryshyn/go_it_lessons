2fe5077160cf0420ed33c46bb8c33f800c55fd00
rpetryshyn <rpetryshyn@amplify.com>
2023-02-27 00:59:18 +0200

    [RP] Added theory

--- L9/index.js
+++ L9/index.js
@@ -1,4 +1,42 @@
-console.log('Executing index.js script...');
+// console.log('Executing index.js script...');
+//
+//
+// ВАЖЛИВІ ТЕОРЕТИЧНІ ПУНКТИ ПО ТЕМІ
+//
+// function doStuff() {}; // function declaration
+// const doStuff = function() {} // function expression
+//
+// ===1===
+// Першо-початково - кожна функція визначається з власним this. У випадку коли ми працюємо
+// в strict mode - this нашої функції буде undefined якщо вона не оголошена всередині якогось конкретного контексту.
+// 'use strict';
+// const foo1 = function() {
+//     console.log(this);
+// }
+// function foo2() {
+//     console.log(this);
+// }
+// // Стрілочні функції не мають власного this, а посилаються на this глобальне (відповідно до контексту)
+// const foo3 = () => {
+//     console.log(this);
+// }
+// foo1(); // undefined if WITH 'use-strict', Window - if WITHOUT use strict
+// foo2(); // undefined if WITH 'use-strict', Window - if WITHOUT use strict
+// foo3(); // Window
+//
+// ===2===
+// Використовуйте .bind(), якщо ви хочете, щоб цю функцію ПІЗНІШЕ викликали з 
+// певним контекстом (через bind ми створюємо копію функції). 
+// Використовуйте .call() або .apply(), якщо ви хочете негайно викликати функцію та змінити контекст.
+// Call/apply викликає функцію негайно, тоді як bind повертає функцію, яка 
+// після виконання пізніше матиме правильний контекст для виклику вихідної функції. 
+// Таким чином ви можете підтримувати контекст в асинхронних зворотних викликах і подіях (детальніше на наступних уроках).
+//
+// ===3===
+// Синтаксис:
+// call(thisArg, arg1, /* …, */ argN)
+// apply(thisArg, argsArray)
+// bind(thisArg, arg1, arg2, /* …, */ argN)
 //
 //
 //
@@ -56,10 +94,12 @@ console.log('Executing index.js script...');
 //
 //
 //
-// 3. Будемо писати об'єкт `calculator` з трьома методами:
+// 3. Будемо писати об'єкт `calculator` з наступними методами:
 // - `read(a, b)`- приймає два значення та зберігає їх як властивості об'єкта.
 // - `add()` - повертає суму збережених значень.
 // - `mult()` - перемножує збережені значення та повертає результат.
+// - `divide(dividerName)` - ділить збереженe поле з ім'ям dividerName на інше збережене поле
+// - `subtract(substractorName)` - віднімає від збереженого поля з ім'ям substractorName інше збережене поле
 // const calculator = {};
 //
 //

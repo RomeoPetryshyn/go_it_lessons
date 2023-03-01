feea03b2462fea49ca20d9bbf18808a4768e775f
rpetryshyn <rpetryshyn@amplify.com>
2023-03-01 21:52:07 +0200

    [RP] Pushed lesson results

--- L9/index.js
+++ L9/index.js
@@ -1,3 +1,4 @@
+// 'use strict';
 // console.log('Executing index.js script...');
 //
 //
@@ -9,7 +10,29 @@
 // ===1===
 // Першо-початково - кожна функція визначається з this відповідно до контексту. У випадку коли ми працюємо
 // в strict mode - контекст нашої функції буде undefined якщо вона не оголошена всередині якогось конкретного контексту.
-// 'use strict';
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+// https://www.w3schools.com/js/js_strict.asp
+// function getNameFunc() {
+//     console.log(this);
+// }
+// const obj1 = {
+//     x: 10,
+//     y: function() {
+//         console.log(this);
+//     }
+// }
+// // getName();
+
+// const obj = {
+//     name: 'Jake',
+//     getName: getNameFunc
+// };
+
+// console.log(obj.getName());
+// console.log(getNameFunc());
+
+// console.log(obj.getName());
+
 // const foo1 = function() {
 //     console.log(this);
 // }
@@ -45,10 +68,16 @@
 // BIND
 // const module = {
 //   x: 42,
-//   getX: function() {
+//   getX: function(testParam) {
+//     console.log('testParam', testParam);
+//     console.log('Our this is: ', this);
 //     return this.x;
 //   }
 // };
+// // module.getX('test');
+// const boundGetX = module.getX.bind({x: 50}, 'TEST2');
+// console.log(boundGetX());
+// console.log(module.getX());
 // console.log(module.getX());
 // const boundGetX = module.getX.bind({x: 50});
 // console.log(boundGetX());
@@ -56,14 +85,20 @@
 //
 //
 //
+//
 // APPLY
 // const module = {
 //   x: 42,
-//   getX: function() {
+//   getX: function(testParam1, testParam2) {
+//     console.log('Params: ', [testParam1, testParam2]);
+//     console.log(this);
 //     return this.x;
 //   }
 // };
-// console.log(module.getX.apply({x: 100}));
+//
+// // console.log(module.getX());
+//
+// console.log(module.getX.apply({x: 100}, ['testParam1', 'testParam2']));
 // console.log(module.getX());
 //
 //
@@ -75,11 +110,22 @@
 //     return this.x;
 //   }
 // };
-// console.log(module.getX.call({x: 100}));
+// console.log(module.getX.call({x: 100}, 'testParam1', 'testParam2'));
 // console.log(module.getX());
 //
 //
 //
+
+// const userJake = {
+//     name: 'Jake',
+//     getUserName() {
+
+//     }
+// }
+
+// userJake.getUserName();
+
+
 // 1. Будемо писати метод `calcTotalPrice(stoneName)`, який приймає назву каменю і
 // розраховує та повертає загальну вартість каменів з таким ім'ям, ціною та
 // кількістю з властивості `stones`.
@@ -87,36 +133,46 @@
 //     stones: [
 //       { name: 'Emerald', price: 1300, quantity: 4 },
 //       { name: 'Diamond', price: 2700, quantity: 3 },
+//       { name: 'Diamond', price: 2700, quantity: 1 },
 //       { name: 'Sapphire', price: 1400, quantity: 7 },
 //       { name: 'Ruby', price: 800, quantity: 2 },
 //     ],
-//     calcTotalPrice(stoneName) {},
+//     calcTotalPrice: function(stoneName) {
+//         return this.stones.reduce((acc, { name, quantity, price }) => {
+//             if (name === stoneName) {
+//                 acc += price * quantity;
+//             }
+//             return acc;
+//         }, 0);
+//     },
 // };
-// console.log(chopShop.calcTotalPrice('Emerald')); // 5200
-// console.log(chopShop.calcTotalPrice('Diamond')); // 8100
-// console.log(chopShop.calcTotalPrice('Sapphire')); // 9800
-// console.log(chopShop.calcTotalPrice('Ruby')); // 1600
+// console.log(chopShop);
+// // console.log(chopShop.calcTotalPrice('Emerald')); // 5200
+// console.log(chopShop.calcTotalPrice('Diamond')); // 10800
+// // console.log(chopShop.calcTotalPrice('Sapphire')); // 9800
+// // console.log(chopShop.calcTotalPrice('Ruby')); // 1600
 //
 //
 //
 // 2. Будемо рефакторити методи об'єкту `phonebook` щоб код запрацював.
 // const phonebook = {
-//   contacts: [],
-//   add(contact) {
-//     const newContact = {
-//       list: 'default',
-//       ...contact,
-//       id: generateId(),
-//       createdAt: getDate(),
-//     };
-//     contacts.push(newContact);
-//   },
-//   generateId() {
-//     return '_' + Math.random().toString(36).substr(2, 9);
-//   },
-//   getDate() {
-//     return Date.now();
-//   },
+//     contacts: [],
+//     add: function(contact) {
+//         const newContact = {
+//             list: 'default',
+//             ...contact,
+//             id: this.generateId(),
+//             createdAt: this.getDate(),
+//         };
+//         this.contacts.push(newContact);
+//         return {allContacts: this.contacts, addedContact: newContact};
+//     },
+//     generateId: function() {
+//         return '_' + Math.random().toString(36).substr(2, 9);
+//     },
+//     getDate: function() {
+//         return Date.now();
+//     }
 // };
 // console.log(
 //   phonebook.add({
@@ -131,16 +187,72 @@
 //     email: 'poly@hotmail.com',
 //   }),
 // );
+
 //
 //
 //
 // 3. Будемо писати об'єкт `calculator` з наступними методами:
-// - `read(a, b)`- приймає два значення та зберігає їх як властивості об'єкта.
+// - `write(a, b)`- приймає два значення та зберігає їх як властивості об'єкта.
 // - `add()` - повертає суму збережених значень.
 // - `mult()` - перемножує збережені значення та повертає результат.
 // - `divide(dividerName)` - ділить збереженe поле з ім'ям dividerName на інше збережене поле
-// - `subtract(substractorName)` - віднімає від збереженого поля з ім'ям substractorName інше збережене поле
-// const calculator = {};
+// const calculator = {
+//     write: function(numberOne, numberTwo) {
+//         this.numberOne = numberOne;
+//         this.numberTwo = numberTwo;
+//     },
+
+//     add: function() {
+//         return this.numberOne + this.numberTwo; 
+//     },
+
+//     mult: function() {
+//         return this.numberOne * this.numberTwo;
+//     },
+
+//     divide: function(dividerName) { 
+//         const firstNumber = this[dividerName];
+//         const secondNumberName = dividerName === 'numberTwo' ? 'numberOne' : 'numberTwo';
+//         const secondNumber = this[secondNumberName];
+//         return firstNumber / secondNumber;
+//         // return this[dividerName] / this[dividerName === 'a' ? 'b' : 'a'];
+//     },
+// };
+
+// calculator.write(10, 5);
+// calculator.divide('numberTwo');
+
+// console.log(calculator.add());
+// console.log(calculator.mult());
 //
 //
 //
+
+
+
+// const obj1 = {
+//     name: 'Jake'
+// };
+
+// obj1.name; // Jake
+// const jakeNameKeyName = 'name';
+// obj1[jakeNameKeyName]; // obj1['name'];
+
+
+
+// const calculator = {
+//     numberOne: 10,
+//     numberTwo: 5,
+
+//     divide: function(firstNumberName, secondNumberName) {
+//         const firstNumber = this[firstNumberName]; // this.numberTwo // 5;
+//         const secondNumber = this[secondNumberName]; // this.numberOne // 10;
+//         return firstNumber / secondNumber;
+//     },
+// };
+
+// calculator.divide('numberTwo', 'numberOne');
+
+
+
+

bd2e80325688c13d3d66236160c5067d858290fe
rpetryshyn <rpetryshyn@amplify.com>
2023-03-22 21:59:41 +0200

    [RP] Added Lesson 13

--- L13/index.js
+++ L13/index.js
@@ -1,5 +1,12 @@
 console.log('Executing index.js script...');
 
+// const divElem = document.querySelector('div');
+// divElem.addEventListener('click', (event) => {
+//     console.log(event);
+// })
+
+
+
 // 0. Розглянемо приклад з конспекту щоб зрозуміти детальніше
 // + stopPropagation() i stopImmediatePropagation()
 //
@@ -7,19 +14,19 @@ console.log('Executing index.js script...');
 // const child = document.querySelector("#child");
 // const descendant = document.querySelector("#descendant");
 // parent.addEventListener("click", (event) => {
+//   console.log("Parent click handler");
 //   console.log("event.target: ", event.target);
 //   console.log("event.currentTarget: ", event.currentTarget);
-//   console.log("Parent click handler");
 // });
 // child.addEventListener("click", (event) => {
+//   console.log("Child click handler");
 //   console.log("event.target: ", event.target);
 //   console.log("event.currentTarget: ", event.currentTarget);
-//   console.log("Child click handler");
 // });
 // descendant.addEventListener("click", (event) => {
+//   console.log("Grandchild click handler");
 //   console.log("event.target: ", event.target);
 //   console.log("event.currentTarget: ", event.currentTarget);
-//   console.log("Descendant click handler");
 // });
 // 
 //
@@ -28,12 +35,25 @@ console.log('Executing index.js script...');
 // 1. Будемо на базі нашої розмітки писати невеличке фортепіано.
 // В нас є наперед готова функція яка викликає звук при нажатті на елемент.
 // Нам потрібно організувати виклик цієї функції при нажатті на кожну окрему кнопку.
-// Спочатку робимо надаючу слухач подій на кожну кнопку - потім переробляємо через 
+// Спочатку робимо надаючи слухач подій на кожну кнопку - потім переробляємо через 
 // event delegation.
-/**
- * @param {Object} key - HTML елемент (клавіша) на яку було натиснуто
- * @returns {void} - нічого не повертає, включаю звуковий ефект на кнопку
- */
+// const user = {
+//     name: "Jake",
+//     lastName: 'Jakeson'
+// }
+// const { name, lastName } = user;
+// const users = ['Jake', 'John'];
+// const [jakeUserName, johnUserName] = users;
+
+// const [ piano ] = document.getElementsByClassName('piano');
+// piano.addEventListener('click', (event) => {
+//     const targetElem = event.target;
+//     playNote(targetElem);
+// });
+// /**
+//  * @param {Object} key - HTML елемент (клавіша) на яку було натиснуто
+//  * @returns {void} - нічого не повертає, включаю звуковий ефект на кнопку
+//  */
 let playNote = (key) => {
     console.dir(key);
     const noteSound = document.getElementById(key.dataset.note);
@@ -47,16 +67,27 @@ let playNote = (key) => {
 //
 //
 //
-// 2. Покращемо наше фортепіано. Зробимо так щоб воно працювало при нажаття на клавішу в будь якій
+// 2. Покращимо наше фортепіано. Зробимо так щоб воно працювало при нажаття на клавішу в будь якій
 // точні нашого веб-додатку.
 // const keys = document.querySelectorAll('.key');
-// const regulars = document.querySelectorAll('.key.white');
-// const sharps = document.querySelectorAll('.key.black');
-// const whites = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
-// const blacks = ['w', 'e', 'r', 't', 'y'];
-// document.addEventListener('keydown', e => {
-//     // playNote(regulars[whiteKeyIndex]) - access white keys
-//     // playNote(sharps[blackKeyIndex]) - access black keys
+// document.addEventListener('keydown', (event) => {
+//     [...keys].forEach((existingButtton) => {
+//         const text = existingButtton.innerText;
+//         const lowerCaseText = text.toLocaleLowerCase();
+//         const eventCodeLower = (event.code[event.code.length - 1]).toLocaleLowerCase();
+//         if (lowerCaseText === eventCodeLower) {
+//             playNote(existingButtton);
+//             return;
+//         }
+//     });
+// });
+// [1, 2, 3, 4, 5].forEach((item) => {
+//     console.log(item, '===1');
+//     if (item === 2) {
+//         console.log(item, '===2');
+//         
+//     }
+//     console.log(item, '===3');
 // });
 //
 //
@@ -65,11 +96,18 @@ let playNote = (key) => {
 // Ми повинні зробити так щоб при натисканні на неї не було 
 // жодного звуку а лише виводився лог що клавіша зламана. 
 // Вспливання евенту повинне припинитись.
-//
-//
-//
-// 4. Будемо намагатись ловити наші евенти при натисканні по рівню вкладеності
-// grandpa, father, son, dog, cat. Спробую всю інформацію скласти і вивести у консоль
+// const [ piano ] = document.getElementsByClassName('piano');
+// piano.addEventListener('click', (event) => {
+//     console.log(event);
+//     const targetElem = event.target;
+//     playNote(targetElem);
+// });
+// const wElem = document.getElementById('w-key-on-piano');
+// wElem.addEventListener('click', (event) => {
+//     console.log(event);
+//     event.stopPropagation();
+//     alert('Jake broken the button!');
+// });
 //
 //
 //
